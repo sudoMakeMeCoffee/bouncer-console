cac72cc871c6fefb9b1f47df5184c9e7d948f7ee
sith <skdev.techno@gmail.com>
2025-09-02 20:23:06 +0530

    feat: fetch all created apps

--- src/components/dashboard/App.jsx
+++ src/components/dashboard/App.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const App = () => {
+const App = ({ app }) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Link
+      to=""
+      className="min-h-[200px] bg-[#212126] rounded-md mt-4 p-4 flex items-center justify-center border border-gray-700"
+    >
+      <span className="text-sm flex items-center gap-2">{app.name}</span>
+    </Link>
+  );
+};
 
-export default App
+export default App;
--- src/pages/dashboard/Apps.jsx
+++ src/pages/dashboard/Apps.jsx
@@ -1,22 +1,48 @@
-import React from "react";
+import React, { use, useEffect, useState } from "react";
 import Home from "../Home";
 import App from "../../components/dashboard/App";
 import { FaPlus } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { API_URL } from "../../Consts";
 
 const Apps = () => {
-  return (
-   <div>
-    <h1 className="text-2xl">Applications</h1>
+  const [apps, setApps] = useState([]);
+  useEffect(() => {
+    getAllApps();
+  }, []);
 
-    <div>
-      <App/>
-      <Link to="/dashboard/apps/new" className="w-[300px] h-[200px] bg-[#212126] rounded-md mt-4 p-4 flex items-center justify-center border border-dotted">
-        <span className="text-sm flex items-center gap-2"><FaPlus/> New Application</span>
-      </Link>
+  const getAllApps = () => {
+    axios
+      .get(`${API_URL}/client/app`, { withCredentials: true })
+      .then((res) => {
+        setApps(res.data.data);
+        console.log(res);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  return (
+    <div className="w-full">
+      <h1 className="text-2xl">Applications</h1>
+
+      <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+        {apps.map((app) => (
+          <App key={app.id} app={app} />
+        ))}
+        <Link
+          to="/dashboard/apps/new"
+          className=" bg-[#212126] rounded-md mt-4 p-4 flex items-center justify-center border border-dotted"
+        >
+          <span className="text-sm flex items-center gap-2">
+            <FaPlus /> New Application
+          </span>
+        </Link>
+      </div>
     </div>
-   </div>
-  )
+  );
 };
 
 export default Apps;
